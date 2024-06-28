f0a4d34badf6da813ad3334510cd0c0a471926b8
TezpayReleaseDispatch <tezpay.release.dispatch@alis.is>
2024-06-28 22:57:41 +0000

    updated-tezpay-version

--- src/__tezpay/constants.lua
+++ src/__tezpay/constants.lua
@@ -11,7 +11,7 @@ return {
         ["payout_wallet_private_key"] = "https://raw.githubusercontent.com/tez-capital/tezpay/main/docs/configuration/payout_wallet_private.sample.key"
     },
     sources = {
-        ["linux-x86_x64"] = "https://github.com/tez-capital/tezpay/releases/download/0.16.0-beta/tezpay-linux-amd64",
-        ["linux-arm64"] = "https://github.com/tez-capital/tezpay/releases/download/0.16.0-beta/tezpay-linux-arm64"
+        ["linux-x86_x64"] = "https://github.com/tez-capital/tezpay/releases/download/0.16.1-beta/tezpay-linux-amd64",
+        ["linux-arm64"] = "https://github.com/tez-capital/tezpay/releases/download/0.16.1-beta/tezpay-linux-arm64"
     }
 }

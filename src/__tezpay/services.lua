d4716a8b6d4c22307cccdba8fad75407edd9becc
v <cryi@tutanota.com>
2024-07-15 22:36:46 +0200

    remove system wide residues with remove

--- src/__tezpay/services.lua
+++ src/__tezpay/services.lua
@@ -21,12 +21,19 @@ local function _remove_all_services()
 
 	local _ok, _systemctl = am.plugin.safe_get("systemctl")
 	ami_assert(_ok, "Failed to load systemctl plugin")
+
 	local _user = am.app.get("user", "root")
-	_systemctl = _systemctl.with_options({ container = _user })
+	local _systemctlUser = _systemctl.with_options({ container = _user })
 
 	for _, service in ipairs(_all) do
 		if type(service) ~= "string" then goto CONTINUE end
-		local _ok, _error = _systemctl.safe_remove_service(service)
+
+		local _ok, _error = _systemctl.safe_remove_service(service) -- remove system wide
+		if not _ok then
+			ami_error("Failed to remove " .. service .. ".service " .. (_error or ""))
+		end
+
+		local _ok, _error = _systemctlUser.safe_remove_service(service)
 		if not _ok then
 			ami_error("Failed to remove " .. service .. ".service " .. (_error or ""))
 		end
